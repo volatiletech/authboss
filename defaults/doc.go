f585b35cfb115c186ef22963e2e6e12cdc74de6a
Aaron L <aaron@bettercoder.net>
2018-02-14 15:11:59 -0800

    Move docs in defaults to defaults.go

--- defaults/doc.go
+++
@@ -1,8 +0,0 @@
-// Package defaults houses default implementations for the very many
-// interfaces that authboss has. It's a goal of the defaults package
-// to provide the core where authboss implements the shell.
-//
-// It's simultaneously supposed to be possible to take as many or
-// as few of these implementations as you desire, allowing you to
-// reimplement where necessary, but reuse where possible.
-package defaults
